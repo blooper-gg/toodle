4f22b1ecd9942f04740a3886b3a67186c6e7d61a
Neil Sarkar <83109+neilsarkar@users.noreply.github.com>
2025-06-24 13:27:52 -0700

    Bump publish script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -6,6 +6,11 @@ if (status.byteLength !== 0) {
   throw new Error("git status is not empty, please commit or stash changes before publishing")
 }
 
+if (!process.env.NPM_TOKEN) {
+  throw new Error("NPM_TOKEN is not set in the environment, publish will fail.")
+}
+
+
 const remoteVersion = await tryP(() => $`npm view ${packageJson.name} version`.text(), "0.0.0")
 const npmVersion = remoteVersion.split(".").map((str) => Number.parseInt(str))
 const packageJsonVersion = packageJson.version.split(".").map((str) => Number.parseInt(str))
@@ -34,6 +39,7 @@ await $`echo ${JSON.stringify(packageJson, null, 2)} > package.json`
 await $`bun run build`
 await $`git tag "${packageJson.version}"`
 await $`git push --tags`
+await Bun.write(".npmrc", `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}`)
 await $`npm publish --no-git-checks`
 
 async function tryP<T>(fn: () => Promise<T>, fallback: T) {
